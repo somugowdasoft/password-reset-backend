21854ae55e9c4ffcc61e8d270d33df738fa793b0
Somu Gowda <somugowda.work@gmail.com>
2024-10-10 22:46:05 +0530

    password reset commit

--- models/User.js
+++ models/User.js
@@ -5,18 +5,18 @@ const bcrypt = require('bcrypt');
 
 //user model
 const userSchema = new mongoose.Schema({
-    email: { type: String, require: true, unique: true },
-    password: { type: String, require: true },
-    resetPasswordToken: String,
-    resetPasswordExpires: Date
+  email: { type: String, require: true, unique: true },
+  password: { type: String, require: true },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date
 });
 
 // Hash password before saving
 userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
-    this.password = await bcrypt.hash(this.password, 10);
-    next();
-  });
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
 
 module.exports = mongoose.model("users", userSchema);
