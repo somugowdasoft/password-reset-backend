50e4bcee55cd7897337a6c27b10b444024dc5f0c
Somu Gowda <somugowda.work@gmail.com>
2024-10-11 08:38:15 +0530

    added frontent netlify url

--- controllers/authController.js
+++ controllers/authController.js
@@ -64,7 +64,7 @@ exports.forgotPassword = async (req, res) => {
     //create mail text
     let text = `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
         Please click on the following link, or paste this into your browser to complete the process:\n\n
-        http://localhost:3000/reset-password/${token}\n\n
+        https://charming-palmier-ef396f.netlify.app/reset-password/${token}\n\n
         If you did not request this, please ignore this email and your password will remain unchanged.\n`
 
     //send mail
